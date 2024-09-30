f24d602ba97c22a9854b922c06873b72d0721b21
julianrasou <julianrasou03@gmail.com>
2024-09-30 23:02:19 +0200

    el jodido 13 que no me sale me voy a volver loco

--- 4_DW CLIENTE/Unidad3/Ejercicio 13/ejercicio13.js
+++ 4_DW CLIENTE/Unidad3/Ejercicio 13/ejercicio13.js
@@ -2,7 +2,7 @@ var miVentana;
 var intervalo;
 function crearVentana(){
 
-    miVentana = window.open("","","width=500,height=200");
+    miVentana = window.open("","","width=500,height=200,top=0,left=0");
     miVentana.document.write("<h3 style=\"text-align: center;\">La ventana que rebota</h3>")
 
     intervalo = setInterval(moverVentana, 10);
@@ -15,21 +15,33 @@ function cerrarVentana(){
 }
 
 function moverVentana(){
-    let velocidadX;
-    let velocidadY;
-    if(miVentana.screenX < (window.innerWidth-miVentana.innerWidth)){
-        velocidadX = 3;
-    }else if(miVentana.screenX < 0){
-        velocidadX = -3;
+    let velocidadX = 3;
+    let velocidadY = 3;
+    let derecha, abajo;
+    if(miVentana.screenX <= (window.outerWidth-miVentana.outerWidth-100)){
+        derecha = false;
+    }else if(miVentana.screenX <= 100){
+        derecha = true;
     }
-    if(miVentana.screenY < (window.innerHeight-miVentana.innerHeight)){
-        velocidadY = 3;
-    }else if(miVentana.screenY < 0){
+    if(miVentana.screenY <= (window.outerHeight-miVentana.outerHeight-100)){
+        abajo = false;
+    }else if(miVentana.screenY <= 100){
+        abajo = true;
+    }
+
+    if(derecha){
+        velocidadX = -3;
+    }else{
+        velocidadX = 3;
+    }
+    if(abajo){
         velocidadY = -3;
+    }else{
+        velocidadY = 3;
     }
 
     if(!miVentana.closed){
-        miVentana.moveBy(velocidadX,velocidadY);
+        miVentana.moveBy(velocidadY,velocidadX);
     }
 
 }
