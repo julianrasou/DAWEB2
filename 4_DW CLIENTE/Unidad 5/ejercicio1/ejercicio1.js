340781758966fcf45d833ab0f8443c2feb747a77
julianrasou <julianrasou03@gmail.com>
2024-10-29 11:01:11 +0100

    2 3 4 js unidad 5

--- 4_DW CLIENTE/Unidad 5/ejercicio1/ejercicio1.js
+++
@@ -1,5 +0,0 @@
-function mostrarPosicion(){
-    document.getElementById("pos").innerHTML = `Posición x: ${event.clientX}<br/>Posición y: ${event.clientY}`;
-}
-
-document.addEventListener("mousemove", mostrarPosicion)
